4ace9c5528e308609febe48dd6f1295da65e3f19
denis <_den_is@mail.ru>
2014-10-06 22:43:17 +0400

    gallery edit fields design started (not complited)

--- www/phplibs/GalleryEditHtmlGetter.php
+++ www/phplibs/GalleryEditHtmlGetter.php
@@ -11,6 +11,33 @@ class  GalleryEditHtmlGetter {
                     <div class=\"pic\"\">
                         <img src=\"{$file}\"/>
                     </div>
+
+                    <div id=\"field_editor_div\">
+                        <form id=\"field_editor_form\">
+                            <div id=\"rus_desc_div\">
+                                <label class=\"field_editor_label\">Desc in russian </label>
+                                <textarea id=\"rus_desc_input\"></textarea>
+                            </div>
+                            <div id=\"eng_desc_div\">
+                                <label class=\"field_editor_label\">Desc in english </label>
+                                <textarea id=\"eng_desc_input\" type=\"text\"></textarea>
+                            </div>
+                            <div id=\"rate\">
+                                <label class=\"field_editor_label\">Rate (start count) </label>
+                                <div id=\"rate_radio_group\">
+                                    <input id=\"rate_input\" name=\"rate_input\" type=\"radio\" value=\"1\"></input>
+                                    <input id=\"rate_input\" name=\"rate_input\" type=\"radio\" value=\"2\"></input>
+                                    <input id=\"rate_input\" name=\"rate_input\" type=\"radio\" value=\"3\"></input>
+                                </div>
+                            </div>
+                            <div id=\"position\">
+                                <label class=\"field_editor_label\">Position </label>
+                                <input id=\"position_input\" type=\"text\"></input>
+                            </div>
+
+                        </form>
+                    </div>
+
                     <div class=\"pic_controls controls\">
                         <div class=\"red remove\">
                             <a class=\"remove_pic\" file_name=\"{$file_name}\" href=\"javascript: void(0)\" > remove</a>
