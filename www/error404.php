60137818c3fa99c497d53fb9f4eaa098ff901c99
denis <_den_is@mail.ru>
2015-04-19 04:28:32 +0300

    error 404 (cherry picked from commit 59d3d3e)

--- www/error404.php
+++ www/error404.php
@@ -1,8 +1,17 @@
 <?php
 
 require_once 'phplibs/ResourceService.php';
+require_once './helpers.php';
+actionHeader();
 
+$actions = array(
+    "common_action" => array ("type" => "everyone")
+);
+
+function common_action_handler() {
     $page = new ErrorPageViewer404();
     $page -> show($_POST);
+}
 
+require_once './router.php';
 ?>
